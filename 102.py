f8d2fa523b1dcd0997a36ce1b7b0f83990919094
Jayneel Pandya <pandyajayneel24@gmail.com>
2019-08-14 11:38:06 +0530

    Update 102.py

--- 102.py
+++ 102.py
@@ -3,11 +3,6 @@
 
 # In[20]:
 
-
-#2: Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional
-#array. The element value in the i-th row and j-th column of the array should be i*j.
-#Note: i=0,1.., X-1; j=0,1,¡-Y-1.
-
 row= int(input('Enter number of rows: '))
 col= int(input('Enter number of columns: '))
 mixlist = [[0 for col in range(col)] for row in range(row)]
